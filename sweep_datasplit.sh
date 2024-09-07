23a792ac83d148bdf6361e36099407db180f3dd2
Jerry Liu <jwl50@stanford.edu>
2024-09-07 19:03:32 +0000

    Update data visualization

--- sweep_datasplit.sh
+++ sweep_datasplit.sh
@@ -6,7 +6,8 @@ gpus=(0 1 2 3)
 
 # Sweep args
 math_operator="x**2+y**2+x*y_mod_97"
-random_seed=0
+random_seed=3
+max_steps=500000
 
 cmd="./scripts/train.py --random_seed ${random_seed} --math_operator ${math_operator}"
 logdir="/scratch/grok/sweep_logs/${math_operator}_seed${random_seed}"
@@ -19,7 +20,7 @@ for ratio in $split_ratios; do
             if ! screen -list | grep -q "gpu${gpu}"; then
                 # Make sure the log directory exists
                 mkdir -p ${logdir}/${ratio}
-                full_cmd="source $(conda info --base)/etc/profile.d/conda.sh && conda activate grok-openai && ${cmd} --gpu ${gpu} --train_data_pct ${ratio} --logdir ${logdir}/${ratio} && exit"
+                full_cmd="source $(conda info --base)/etc/profile.d/conda.sh && conda activate grok-openai && ${cmd} --gpu ${gpu} --train_data_pct ${ratio} --logdir ${logdir}/${ratio} --max_steps ${max_steps} && exit"
                 screen -dmS "gpu${gpu}_task_${ratio}" bash -c "${full_cmd}"
                 echo "Started job with split_ratio ${ratio} on GPU ${gpu} in screen session gpu${gpu}_task_${ratio}"
                 sleep 1  # Small delay to avoid potential race conditions
